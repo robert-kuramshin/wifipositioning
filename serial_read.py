aee3f0a047b503a69375379b5afd302e1db63411
robert-kuramshin <robert.kuramshin98@gmail.com>
2017-06-17 14:12:48 +0000

    getting gps working and decoding data

---
+++ serial_read.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import time
+import serial
+from pynmea import nmea
+
+
+ser = serial.Serial(
+    port='/dev/serial0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+counter = 0
+gpgga = nmea.GPGGA()
+
+while 1:
+    x=ser.readline()
+    if (x.startswith('$GPGGA')):
+        gpgga.parse(data)
+        print 'Lat: ',gpgga.latitude
+        print 'Long: ', gpgga.longitude
+        print 'Alt: ', gpgga.antenna_altitudes,' ', gpgga.altitude_units
+        
