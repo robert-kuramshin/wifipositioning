5c041935f9589bb969ea20343aabbbf1c031706f
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-06-27 21:45:49 -0400

    more db_insertion functionality

--- populate_db.py
+++ populate_db.py
@@ -1,5 +1,44 @@
 from pymongo import MongoClient
 
+def address_first_json(array):
+    json = []
+    by_address = {}
+    for element in array:
+        for network in element[1]:
+            address=network[0]
+            if address not in by_address.keys():
+                by_address[address]=[[element[0],network]]
+            else:
+                by_address[address].append([element[0],network])
+    print by_address.keys()
+    json=[]
+    for key in by_address.keys():
+        address = key
+        networks =[]
+        for element in by_address[key]:
+            network = element[1]
+            network_json = {
+            "Address":network[0],
+            "Signal":network[1],
+            "Quality":network[2]
+            }
+            coordinates_json = {
+            "lat":element[0][0],"lon":element[0][1],"alt":element[0][2],"sat":element[0][3]
+            }
+            pair_json = {
+                "coordinates":coordinates_json,
+                "network":network_json
+            }
+            networks.append(pair_json)
+        json.append({
+        "address":address,
+        "networks": networks
+        })
+    return json
+    
+
+    
+                
 def create_json(array):
     json=[]
     for element in array:
@@ -10,15 +49,14 @@ def create_json(array):
                 "Signal":network[1],
                 "Quality":network[2]
             })
+        coordinates = {
+            "lat":element[0][0],"lon":element[0][1],"alt":element[0][2],"sat":element[0][3]
+        }
         json.append({
-            "coordinates":element[0],
+            "coordinates":coordinates,
             "networks":networks
         })
     return json 
-def networks_json(pairs):
-    networks=
-    for pair in pairs:
-        for wifi in par[1]:
 
 def parse_file(name):
     pairs = []
@@ -78,5 +116,9 @@ collection = db.data_pairs
 
 
 pairs = parse_file("values.txt")
-json = create_json(pairs)
+#json = create_json(pairs)
+#collection.insert_many(json)
+
+json = address_first_json(pairs)
+collection = db.by_address
 collection.insert_many(json)
